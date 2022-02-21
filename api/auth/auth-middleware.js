49d4dfbc4bf2a0ba9f76a7be07479fb9c6cb31b9
DeQuavion Wilburn <qua.thesage19@gmail.com>
2022-02-21 14:45:52 -0600

    all test passing

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,5 @@
+const Users = require("../users/users-model")
+const bcrypt = require("bcryptjs")
 /*
   If the user does not have a session saved in the server
 
@@ -25,29 +27,48 @@ function restricted(req, res, next) {
     "message": "Username taken"
   }
 */
-function checkUsernameFree() {}
+async function checkUsernameFree(req, res, next) {
+  const [username] = await Users.findBy({ username: req.body.username })
+  if (username !== undefined) {
+    next({ status: 422, message: "Username taken" })
+  } else {
+    next()
+  }
+}
 
 /*
   If the username in req.body does NOT exist in the database
-
   status 401
   {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists() {}
+async function checkUsernameExists(req, res, next) {
+  const { username, password } = req.body
+  const [existing] = await Users.findBy({ username })
+
+  if (existing && bcrypt.compareSync(password, existing.password)) {
+    req.user = existing
+    next()
+  } else {
+    next({ status: 401, message: "Invalid credentials" })
+  }
+}
 
 /*
   If password is missing from req.body, or if it's 3 chars or shorter
-
   status 422
   {
     "message": "Password must be longer than 3 chars"
   }
 */
-function checkPasswordLength() {}
-
-// Don't forget to add these to the `exports` object so they can be required in other modules
+function checkPasswordLength(req, res, next) {
+  if (!req.body.password || req.body.password.length <= 3) {
+    next({ status: 422, message: "Password must be longer than 3 chars" })
+  } else {
+    next()
+  }
+}
 
 module.exports = {
   checkPasswordLength,
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,38 +1,57 @@
-// Require `checkUsernameFree`, `checkUsernameExists` and `checkPasswordLength`
-// middleware functions from `auth-middleware.js`. You will need them here!
-
+const router = require("express").Router()
+const User = require("../users/users-model")
+const bcrypt = require("bcryptjs")
+const {
+  checkUsernameFree,
+  checkPasswordLength,
+  checkUsernameExists,
+} = require("./auth-middleware")
 
 /**
   1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
   response:
   status 200
   {
     "user_id": 2,
     "username": "sue"
   }
-
   response on username taken:
   status 422
   {
     "message": "Username taken"
   }
-
   response on password three chars or less:
   status 422
   {
     "message": "Password must be longer than 3 chars"
   }
  */
+router.post(
+  "/register",
+  checkUsernameFree,
+  checkPasswordLength,
+  async (req, res, next) => {
+    try {
+      const { username, password } = req.body
+      const hash = bcrypt.hashSync(password, 8)
 
+      const newUser = { username, password: hash }
+
+      const insertedUser = await User.add(newUser)
+      res.status(200).json(insertedUser)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
 
 /**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
+ 2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
 
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
+ response:
+ status 200
+ {
+   "message": "Welcome sue!"
   }
 
   response on invalid credentials:
@@ -40,8 +59,16 @@
   {
     "message": "Invalid credentials"
   }
- */
+  */
 
+router.post("/login", checkUsernameExists, (req, res, next) => {
+  try {
+    req.session.user = req.user
+    next({ status: 200, message: `Welcome ${req.user.username}!` })
+  } catch (err) {
+    next("error", err)
+  }
+})
 
 /**
   3 [GET] /api/auth/logout
@@ -57,7 +84,22 @@
   {
     "message": "no session"
   }
- */
+  */
+router.get("/logout", (req, res, next) => {
+  if (req.session.user) {
+    req.session.destroy((err) => {
+      if (err) {
+        res.json({
+          message: "Sorry could you retry",
+        })
+      } else {
+        next({ message: "logged out" })
+      }
+    })
+  } else {
+    next({ message: "no session" })
+  }
+})
 
- 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
+module.exports = router
